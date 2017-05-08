447fbf51778448da7de5b48ad3553d01bd02931f
ikkanlaz <andrew.lazenby@gmail.com>
2017-05-07 19:35:02 -0700

    changing game start input

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -1,4 +1,6 @@
 // Let's start by grabbing a reference to the <span> below.
+var startText = document.getElementById("start-game");
+var gameScreen = document.getElementById("game-screen");
 var wins = document.getElementById("wins");
 var wordToGuess = document.getElementById("word-to-guess");
 var numberOfGuesses = document.getElementById("number-of-guesses");
@@ -12,36 +14,43 @@ var userInput;
 console.log(wins.style);
 
 function setGameSceen() {
-    document.getElementById("any-key").style = "display: none;";
-    document.getElementById("game-screen").style = "display: block;";
+    startText.style = "display: none;";
+    gameScreen.style = "display: block;";
 }
 
 function startGame() {
     wordToGuess = getWordToGuess();
 }
 
-function getWordToGuess (){
-    
+function getWordToGuess() {
+
 }
 
 function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+
+
 document.onkeyup = function (event) {
-    if(isGameStarted === false) {
+    if (isGameStarted === true) {
+        userText.textContent = event.key;
+        userInput = userText.textContent;
+        console.log(userInput);
+        randomWord = computerChoices[getRandomArbitrary(0, 3)];
+        computerGuess.textContent = randomWord;
+        console.log("randomWord: " + randomWord);
+    }
+};
+
+document.onclick = function (event) {
+    if (isGameStarted === false) {
         setGameSceen();
         startGame();
         isGameStarted = true;
     }
-    userText.textContent = event.key;
-    userInput = userText.textContent;
-    console.log(userInput);
-    randomWord = computerChoices[getRandomArbitrary(0, 3)];
-    computerGuess.textContent = randomWord;
-    console.log("randomWord: " + randomWord);
+}
 
-};
 
 
 	/*1. Get input from player to start game loop
