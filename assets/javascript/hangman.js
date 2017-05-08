6a559341783b30b7a2c89e05103abf115722f21e
ikkanlaz <andrew.lazenby@gmail.com>
2017-05-07 19:13:56 -0700

    adding styles and part of javascript

---
+++ assets/javascript/hangman.js
@@ -0,0 +1,51 @@
+// Let's start by grabbing a reference to the <span> below.
+var wins = document.getElementById("wins");
+var wordToGuess = document.getElementById("word-to-guess");
+var numberOfGuesses = document.getElementById("number-of-guesses");
+var guessedLetters = document.getElementById("guessed-letters");
+var usedWords = [];
+var isGameStarted = false;
+var winCount = 0;
+var randomWord;
+var wordChoices = ["r", "p", "s"];
+var userInput;
+console.log(wins.style);
+
+function setGameSceen() {
+    document.getElementById("any-key").style = "display: none;";
+    document.getElementById("game-screen").style = "display: block;";
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function startGame() {
+    wordToGuess = getWordToGuess();
+}
+
+document.onkeyup = function (event) {
+    if(isGameStarted === false) {
+        setGameSceen();
+        startGame();
+        isGameStarted = true;
+    }
+    userText.textContent = event.key;
+    userInput = userText.textContent;
+    console.log(userInput);
+    randomWord = computerChoices[getRandomArbitrary(0, 3)];
+    computerGuess.textContent = randomWord;
+    console.log("randomWord: " + randomWord);
+
+};
+
+
+	/*1. Get input from player to start game loop
+		a. Initialize game
+			i. Generates random word
+			ii. Sets values on html to default (if necessary) and add "current word spaces"
+	2. Player guesses a letter
+		a. Check to see if letter matches a letter in word
+			i. If true, then add letter to all instances of letter in "current word spaces"
+			ii. If false, subtract 1 from number of guesses remaining
+			iii. In both instances, add letter to "letters already guessed"*/
